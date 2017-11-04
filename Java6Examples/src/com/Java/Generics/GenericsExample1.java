a39bba28aaad392f448f2f6032c8ea05b2a84edb
santosh.s.jawale <santosh.s.jawale@M5-L-5024XJL.dir.svc.accenture.com>
2017-11-04 15:25:14 +0530

    First Commit

---
+++ Java6Examples/src/com/Java/Generics/GenericsExample1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.Java.Generics;
+
+/**
+ * @author santosh.s.jawale
+ *
+ */
+public class GenericsExample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
